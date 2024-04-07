0b435b39356b9b61a1fee03c15d749e111d7ed83
Armando <jarodriguezvera@gmail.com>
2024-04-07 01:34:17 -0600

    DATA BASE EXERCISE

--- app/src/main/java/com/mexiti/cronoapp/viewmodel/DataViewModel.kt
+++ app/src/main/java/com/mexiti/cronoapp/viewmodel/DataViewModel.kt
@@ -7,13 +7,16 @@ import com.mexiti.cronoapp.repository.CronosRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DataViewModel @Inject constructor(
+
     private val repository:CronosRepository):ViewModel() {
         private val _cronoList = MutableStateFlow<List<Cronos>>(emptyList())
+    val cronolist= _cronoList.asStateFlow()
     init {
         viewModelScope.launch(Dispatchers.IO) {
 
